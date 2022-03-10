be072f1f655d87639af15968c7358c91a145d451
MaciekCelinski <mac.celinski@gmail.com>
2022-03-10 14:00:41 +0100

    registration page e03 - 25min

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,19 @@ import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
+import Header from "./components/Header";
+
 function App() {
 	return (
 		<>
 			<Router>
 				<div className="container">
-          <Routes>
-            <Route path="/" element={<Dashboard/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/register" element={<Register/>}/>
-          </Routes>
+					<Header />
+					<Routes>
+						<Route path="/" element={<Dashboard />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/register" element={<Register />} />
+					</Routes>
 				</div>
 			</Router>
 		</>
